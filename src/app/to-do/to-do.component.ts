98887768d00b931157cbe31854c77c3659763e01
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-05-01 11:38:07 -0400

    added light and dark theme

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NewTaskComponent } from '../task/new-task/new-task.component';
 import { TaskComponent } from '../task/task.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [NewTaskComponent,TaskComponent, TaskComponent],
+  imports: [CommonModule, NewTaskComponent,TaskComponent, TaskComponent],
   templateUrl: './to-do.component.html',
 
 })
 export class ToDoComponent {
+  theme 
+  light = {
+    type: "Light",
+    symbol: "\u263E"
+  }
+  Dark = {
+    type: "Dark",
+    symbol: "\u263C"
+  }
+  constructor(){
+    this.theme = this.Dark;
+  }
+
+  setTheme(){
+
+  }
+  toggleTheme(){
+    if(this.theme.type === "Light" )
+      this.theme = this.Dark;
+    else if (this.theme.type === "Dark")
+      this.theme=this.light;
+    
+  }
 
 }
